69dda916665f7fdf7f4631b1b3dfe182e650b872
Thomics <contot@gmail.com>
2016-06-15 22:33:15 -0700

    objects to arrays

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,10 +3,13 @@
 angular.module('bookApp')
   .controller('mainCtrl', function( $scope, MainService ) {
 
-    MainService.getBooks(function(res) {
-      console.log(res.data.docs[0]);
-      $scope.books = res.data.docs[0];
-      $scope.title = res.data.docs[0].title;
+    MainService.getBooks( function( res ) {
+      MainService.createBookLibrary( res, function (data) {
+        console.log( data );
+        console.log(data.title);
+        $scope.books = data;
+        $scope.title = data.title;
+      });
     });
 
 });
--- app/scripts/services/main.js
+++ app/scripts/services/main.js
@@ -2,8 +2,25 @@
 
 angular.module('bookApp')
 .service( 'MainService', function ( $http ) {
+
   this.getBooks = function( callback ) {
     $http.get("https://openlibrary.org/search.json?q=Name+of+the+wind").then( callback );
+
+  };
+
+  this.createBookLibrary = function ( data, callback ) {
+
+    var bookObj = {};
+
+    bookObj["title"] = data.data.docs[0].title_suggest;
+    bookObj["isbn"] = data.data.docs[0].isbn[0];
+    bookObj["image"] = "http://covers.openlibrary.org/b/isbn/" + data.data.docs[0].isbn[0] + "-M.jpg";
+    bookObj["reviews"] = [{}];
+    bookObj["description"] = "Write a description? Or check back soon.";
+
+    var arr = [bookObj];
+
+    callback(arr);
   };
 
 
