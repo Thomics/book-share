6105c20215393e1d5d975511bb0803be557038e4
Thomics <contot@gmail.com>
2016-06-17 23:02:28 -0700

    working search

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,31 +1,15 @@
 'use strict';
 
 angular.module('bookApp')
-  .controller('mainCtrl', function( $scope, MainService ) {
+  .controller('mainCtrl', function( $scope, BookService ) {
 
-    //$scope.books = [];
-    //
-    //MainService.getBooks( function ( data ) {
-    //
-    //
-    //  var books = {
-    //    title : data.data.docs[0].title_suggest,
-    //    isbn : data.data.docs[0].isbn[0],
-    //    image : "http://covers.openlibrary.org/b/isbn/" + data.data.docs[0].isbn[0] + "-M.jpg",
-    //    reviews : [{}],
-    //    description : "Write a description? Or check back soon."
-    //  };
-    //
-    //  $scope.books.push(books);
-    //
-    //});
     $scope.books = [];
 
     var arr = ["The+Count+of+Monte+Cristo", "Name+of+the+Wind",
       "I+Am+Legend", "Surely+You're+Joking+Mr.+Feynman"];
 
     for ( var i = 0; i < arr.length; i++ ) {
-      MainService.getBooks(arr[i])
+      BookService.getBooks(arr[i])
         .success(function(data) {
 
           var bookObj = {
@@ -37,8 +21,8 @@ angular.module('bookApp')
           };
 
           $scope.books.push(bookObj);
-          console.log(bookObj.title);
-          console.log($scope.books);
+          //console.log(bookObj.title);
+          //console.log($scope.books);
 
         }).error(function(err) {
           console.log(err);
@@ -46,5 +30,30 @@ angular.module('bookApp')
     }
 
 
+    $scope.newBook = 'hi';
+
+    $scope.getNewBook = function(book) {
+      console.log('hello');
+
+      BookService.getBooks(book)
+        .success(function(data){
+          //console.log(data);
+
+          var bookObj = {
+            title : data.docs[i].title_suggest,
+            isbn : data.docs[i].isbn[0],
+            image : "http://covers.openlibrary.org/b/isbn/" + data.docs[i].isbn[0] + "-M.jpg",
+            reviews : [{}],
+            description : "No description. Write one."
+          };
+
+          $scope.books.push(bookObj);
+
+        }).error(function(err) {
+
+        });
+
+      //console.log(book);
+    }
 
 });
--- app/scripts/services/main.js
+++ app/scripts/services/main.js
@@ -1,37 +1,11 @@
 'use strict';
 
 angular.module('bookApp')
-  .service( 'MainService', function ( $http, $q ) {
-
-    //var arr = ["The+Count+of+Monte+Cristo", "Name+of+the+Wind",
-    //  "You+Are+a+Badass", "Surely+You're+Joking+Mr.+Feynman"];
-    //
-    //this.getBooks = function( callback ) {
-    //  //for ( var i = 0; i < arr.length; i++ ) {
-    //    $http.get("https://openlibrary.org/search.json?q=" + arr[0]).then(callback);
-    //  //}
-    //};
-
-    //this.createBookLibrary = function ( data, callback ) {
-    //
-    //  var books = {
-    //    title : data.data.docs[0].title_suggest,
-    //    isbn : data.data.docs[0].isbn[0],
-    //    image : "http://covers.openlibrary.org/b/isbn/" + data.data.docs[0].isbn[0] + "-M.jpg",
-    //    reviews : [{}],
-    //    description : "Write a description? Or check back soon."
-    //  };
-    //
-    //  callback(books);
-    //};
-
-
+  .service( 'BookService', function ( $http, $q ) {
 
     this.getBooks = function( title ) {
         return $http.get("https://openlibrary.org/search.json?q=" + title);
     };
 
-
-
-
 });
+
