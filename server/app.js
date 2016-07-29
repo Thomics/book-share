e438b249f385057e073293486323952224d3d6be
Thomics <contot@gmail.com>
2016-07-28 22:54:37 -0700

    working on setting up user authentication

--- server/app.js
+++ server/app.js
@@ -5,8 +5,12 @@ var parser = require('body-parser');
 var app = express();
 var router = require('./api/index');
 var methodOverride = require('method-override');
+var passport = require('passport');
 
 var port = process.env.PORT || 3000;
+
+var routesApi = require('./routes/index');
+
 
 require('./database');
 
@@ -14,8 +18,30 @@ app.use('/', express.static('public'));
 
 app.use(parser.json());
 
+//new
+require('./models/users');
+require('./config/passport');
+app.use(passport.initialize());
+//end new
+
+
 app.use('/api', router);
+
+// [SH] Use the API routes when path starts with /api
+app.use('/api', routesApi);
+
+
 
 app.listen(port, function() {
   console.log('Server on port:' + port);
+});
+
+
+// error handlers
+// Catch unauthorised errors
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message" : err.name + ": " + err.message});
+  }
 });
---
+++ server/models/users.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+var crypto = require('crypto');
+
+var userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  hash: String,
+  salt: String
+});
+
+//Code to save and obfuscate users password. (From tutorial)
+userSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+//Code to validate users password (From tutorial)
+userSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
+
+//Generates a json web token that follows the user from page to page. (From tutorial)
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, "MY_SECRET");
+};
+
+mongoose.model('User', userSchema);
