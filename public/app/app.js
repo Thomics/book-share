c9d75602cab1ccbe2a69e971c8d27bef7aa253ea
Thomics <contot@gmail.com>
2016-10-13 00:26:19 -0700

    working on setting up chat

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,7 @@
  * @module
  **/
 
-angular.module('bookApp', ['ngRoute', 'ui.bootstrap', 'ngAnimate'])
+angular.module('bookApp', ['ngRoute', 'ui.bootstrap', 'ngAnimate', 'ngLoadScript'])
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
 'use strict';
